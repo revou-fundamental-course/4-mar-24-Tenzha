d85478055c415afda05e9d274e62fbfbdafb68c4
Pandu <yb24875@gmail.com>
2024-03-12 22:17:31 +0700

    Final deployment

--- js/main.js
+++ js/main.js
@@ -6,6 +6,7 @@ document.addEventListener('DOMContentLoaded', function () {
     const heightInput = document.getElementById('height');
     const ageInput = document.getElementById('age');
     const genderInputs = document.querySelectorAll('input[name="gender"]');
+    const resultPopup = document.getElementById('result-popup');
 
     calculateButton.addEventListener('click', function () {
         const gender = document.querySelector('input[name="gender"]:checked');
@@ -16,16 +17,30 @@ document.addEventListener('DOMContentLoaded', function () {
         if (gender && !isNaN(weight) && !isNaN(height) && !isNaN(age)) {
             let bmi = weight / (height * height);
             bmiResult.textContent = `Your BMI Result: ${bmi.toFixed(2)}`;
+
+            // Display appropriate message based on BMI result
+            if (bmi < 18.5) {
+                resultPopup.textContent = 'Anda kekurangan berat badan';
+            } else if (bmi >= 18.5 && bmi <= 24.9) {
+                resultPopup.textContent = 'Berat badan anda normal';
+            } else if (bmi >= 25.0 && bmi <= 29.9) {
+                resultPopup.textContent = 'Berat badan anda berlebihan';
+            } else {
+                resultPopup.textContent = 'Anda Obesitas';
+            }
         } else {
-            bmiResult.textContent = 'Please fill in all the fields.';
+            bmiResult.textContent = '';
+            resultPopup.textContent = 'Please fill in all the fields and calculate your BMI.';
         }
     });
 
+    // reset button functionality
     resetButton.addEventListener('click', function () {
         weightInput.value = '';
         heightInput.value = '';
         ageInput.value = '';
         bmiResult.textContent = '';
+        resultPopup.textContent = '';
         genderInputs.forEach(input => (input.checked = false));
     });
-});
+})
